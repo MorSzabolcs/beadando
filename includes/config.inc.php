a7559013eb3bd91dff18b96f0b4b808da43ab295
MorSzabolcs <mor.szbolcs@gmail.com>
2022-11-22 21:28:19 +0100

    Főoldal és galéria egy része

---
+++ includes/config.inc.php
@@ -0,0 +1,35 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Beadando',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.jpg',
+    'kepalt' => 'logo',
+);
+
+$lablec = array(
+    'copyright' => '',
+    'ceg' => ''
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'BEMUTATKOZÁS', 'menun' => array(1,1)),
+	'tamogasson' => array('fajl' => 'tamogasson', 'szoveg' => 'TÁMOGASSON', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'GALÉRIA', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'KAPCSOLAT', 'menun' => array(1,1)),
+    'eredeti' => array('fajl' => 'eredeti', 'szoveg' => 'EREDETI OLDAL', 'menun' => array(1,0)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+
+$MAPPA = './kepek/';
+$TIPUSOK = array ('.jpg', '.png');
+$MEDIATIPUSOK = array('image/jpeg', 'image/png');
+$DATUMFORMA = "Y.m.d. H:i";
+$MAXMERET = 200*300;
+?>
---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div id="header">
+            <img src="images/kep.jpg" alt="kep"><br><br><br>
+        </div>
+                <div id="wrapper">
+                    <div id="sidebar1">
+                        <div class="module">
+                            <h3>Hírlevelek</h3>
+                            <ul class="list">
+                                <li><a href="https://www.ecolinst.hu/index.php/15-hrl/80-hrl2-3">Hírlevél 2020/04</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/15-hrl/79-hrl2-2">Hírlevél 2020/03</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/15-hrl/58-hrl2">Hírlevél 2020/02</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/15-hrl/57-hrl1">Hírlevél 2020/01</a></li>
+                            </ul>
+                        </div>
+                        <div class="module">
+                            <h3>Tudástár</h3>
+                            <ul class="list">
+                                <li><a href="https://www.ecolinst.hu/index.php/gomszoll">Környezetpolitika</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/med">Médiából</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/fil">Filmek</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/sza">Szakanyagok</a></li>
+                            </ul>
+                        </div>
+                        <div class="module">
+                            <h3>Lezárt Projektek</h3>
+                            <ul class="list">
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/26-climate">Climate Needs Farmers</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/25-valtozatos">A változatosság az élet</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/24-esto">ESTO projekt</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/23-kozertek">Közösségek helyi értékei Európa tükrében</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/22-szolgfejl">Szolgáltatás-fejlesztéssel a fenntartható fejlődésért</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/21-infokozp">Gömörszőlősi információs központ</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/20-svajci">Svájci-Magyar Együttműködési program</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/19-fenntarthato">Fenntartható életmód tudásátadás Gömörszőlősön</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/18-vidtaj">Vidéki táj és tájfajták védelme</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/17-gomorf">Gömör földje</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/16-kornypol">Környezetpolitikai döntések elősegítése</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/15-herman">Herman Ottó nyomában</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/14-avason">Környezeti tudatosság fejlesztése az Avason</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/10-lezproj/12-etalajok">Vidéktől a nagyvárosig - az egészséges talajokért!</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="content2">
+                        <div>
+                            <h2>Bemutatkozás</h2><br>
+                            <p>Az Ökológiai Intézet a Fenntartható Fejlődésért Alapítvány célja a fenntartható fejlődés szellemiségének terjesztése, az ökológiai kultúra fejlesztése, a globális szemléletre való nevelés, s mindezeken keresztül a fenntartható fejlődés gyakorlati alapjainak kidolgozása és megvalósulásának elősegítése.</p><br>
+                        </div>
+                        <div> 
+                            <p>
+                                FŐ TEVÉKENYSÉGEINK: 
+                                <br>-a környezet állapotának folyamatos nyomon követése, elemzése és értékelése
+                                <br>-a természeti erőforrások fenntartható használati módjának kifejlesztése és terjesztése,
+                                <br>-az adott természeti viszonyokhoz illeszkedő gazdálkodási rendszerek tervezése, megvalósításuk elősegítése
+                                <br>-az ökológiai kultúra fejlesztése, ökológiai szemléletre nevelés, ökológiai oktatás elősegítése
+                                <br>-ökológiai fogyasztóvédelem
+                            </p>
+                            <p><img src="images/logo2.jpg" width="80%" height="80%" alt="logo2"></p><br><br>
+                            <p>
+                                <strong>Ökológiai Intézet a Fenntartható Fejlődésért Alapítvány</strong>
+                                <br>3525 <i>Miskolc, </i>Kossuth utca 13.
+                            </p>
+                            <p>
+                                <strong>Gömöri Oktató Központ</strong>
+                                <br>3728 <i>Gömörszőlős, </i>Kassai utca 37-39.
+                            </p>
+                            <p>Telefon: (20) 407-6875</p>
+                            <p>Email: okologiai.intezet@gmail.com, rea.vigh@gmail.com</p><br>
+                            <p>
+                                <strong>Miskolci Központ</strong>
+                                <br>Az Ökológiai Intézet Alapítványnál nemrégiben bővítettük ki tevékenységünket, s az udvarban (Kossuth u. 13.) megnyitott bemutató házunkban kézzel fogható módon, s nem csupán tájékoztatással segítjük a jövőben a környezettudatos életmód iránt érdeklődőket. A mini, de komplett háztartás akadálymentes, azaz a kismamáknak, időseknek, a mozgásukban korlátozottaknak sem jelent akadályt a megtekintése.
+                            </p><br><br>
+                            <p><strong>MUNKATÁRSAK</strong></p>
+                            <p>
+                                <strong>DR. GYULAI IVÁN</strong>
+                                <br>gyulai@ecolinst.hu
+                                <br>Ökológus, biológiai-kémia szakos tanár, az Ökológiai Intézet a Fenntartható fejlődésért Alapítvány elnöke, az Intézet igazgatója. Címzetes egyetemi docens a Szent István Egyetemen a "fenntartható fejlődés elmélete és gyakorlata" tárgyat tanítja. Tagja a Nemzeti Fenntartható Fejlődési Tanácsnak. Számos, a fenntartható fejlődés szemléletét terjesztő filmje található az interneten, a legismertebb a "Zsigerbeszéd". Neve összefonódott a "Gömörszőlősi fenntartható falu" programmal.
+                                <br><br><strong>VIGH ANDREA</strong>
+                                <br>rea.vigh@gmail.com
+                                <br>(20) 407-6875
+                                <br>Pedagógus, gyermekpszihológus. 2011. januárjától az Alapítvány munkatársa. Az oktatási programhoz kapcsolódó projektekben dolgozik; projektkoordináror, projektmanager, képzések, előadások vezetője.
+                            </p><br><br>
+                        </div>
+                        <div>
+                            <h2>Elhelyezkedés</h2><br>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d811.4000581230697!2d20.42773011351258!3d48.37353077909861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473f9d32fe4e2cfd%3A0x8e08f46e7f1b32fb!2zw5Zrb2zDs2dpYWkgSW50w6l6ZXQgQWxhcMOtdHbDoW55IE9rdGF0w7MgS8O2enBvbnRqYQ!5e1!3m2!1shu!2shu!4v1668202478794!5m2!1shu!2shu" width="550" height="350" style="border:0;"></iframe><br><br>
+                        </div>
+                        <div>
+                            <br><br><h2>Videók</h2><br>
+                            <video width="550px" height="auto" controls>
+                                <source src="video/video.mp4" type="video/mp4">
+                            </video><br><br>
+                            <iframe src="https://www.youtube.com/embed/6I7_fAYynsQ" width="550px" height="350px"></iframe>
+                        </div>
+                    </div>
+                    <div id="sidebar2">
+                        <div class="module">
+                            <h3>Pályázatok</h3>
+                            <div class="palyazatok">
+                                <p><a href="https://www.ecolinst.hu/index.php/mkoze/9-futoprojekt/64-top-gomor-szive-sziv-utjai"><img src="images/palyazat1.jpg" alt="palyazat" width="210px" height="auto"></a></p>
+                                <p><a href="https://www.ecolinst.hu/index.php/mkoze/9-futoprojekt/13-eforras"><img src="images/palyazat2.jpg" alt="palyazat" width="210px" height="auto"></a></p>
+                                <p><a href="https://www.ecolinst.hu/index.php/mkoze/9-futoprojekt/66-kehop"><img src="images/palyazat3.png" alt="palyazat" width="210px" height="auto"></a></p>
+                            </div>
+                        </div>
+                        <div class="module">
+                            <h3>Aktualitások</h3>
+                            <ul class="list">
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/90-pohanka-vendeghaz-atadas">Új, magas minőségű szálláshellyel bővült az intézeti infrastruktúra</a><br><span>2021.09.21</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/89-somosi-eletmod-projekt">Zárul a SOMOSI ÉLETMÓD... elnevezésű projekt</a><br><span>2021.04.09</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/87-fenntarthato-fejlodes-hetvege">Egy hétvége a fenntarthatóság jegyében</a><br><span>2020.10.23</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/86-komposztfutesu-meleghaz">A komposztfűtésű melegház építése</a><br><span>2020.10.23</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/85-ideje-mulcsolni">Ideje mulcsolni!</a><br><span>2020.10.23</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/84-a-napenergia-hasznositasa">A napenergia hasznosítása</a><br><span>2020.10.23</span></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/11-hirek/83-fenntarthatosagi-hatasvizsgalatok">Fenntarthatósági hatásvizsgálatok - Egyetemi tananyag mindenkinek</a><br><span>2020.10.16</span></li>
+                            </ul>
+                        </div>
+                        <div class="module">
+                            <h3>Futó Projektek</h3>
+                            <ul class="list">
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/91-zold-forras-ptkf-329-2020">Zöld Forrás</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/88-pohanka-vendeghaz">Pohánka Vendégház</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/74-noplanetb">Nincs B-Bolygó!</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/61-core-orchard">CORE projekt - A hagyományos gyümölcsösökért</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/64-top-gomor-szive-sziv-utjai">TOP - Gömör Szíve - Szív útjai</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/66-kehop">KEHOP - Energiatudatos életmód Gömörben</a></li>
+                                <li><a href="https://www.ecolinst.hu/index.php/9-futoprojekt/13-eforras">EFOP - Az élet forrása</a></li>
+                            </ul>
+                        </div>
+                        <div class="module">
+                            <h3>Partnereink</h3>
+                            <div class="partnereink">
+                                <p><a href="https://www.expodom.hu"><img src="images/partnerek1.jpg" width="210px" height="auto"></a></p>
+                                <p><a href="https://bpdigital.hu"><img src="images/partnerek2.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://fuvar.hu"><img src="images/partnerek3.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://mersz.hu"><img src="images/partnerek4.jpg" width="210px" height="auto"></a></p>
+                                <p><a href="https://www.szotar.net"><img src="images/partnerek5.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://clinident.hu"><img src="images/partnerek6.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://benu.hu"><img src="images/partnerek7.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://jatekhotel.hu"><img src="images/partnerek8.png" width="210px" height="auto"></a></p>
+                                <p><a href="https://solar-kit.hu"><img src="images/partnerek9.png" width="210px" height="auto"></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </body>
+</html>
