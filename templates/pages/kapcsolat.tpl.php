ecb0cd6dfa000f989ddd046116f1f16d57c30798
MorSzabolcs <mor.szabolcs10@gmail.com>
2022-11-25 21:14:39 +0100

    4-es feladat második fele

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,13 +1,15 @@
 <h2>Kapcsolat</h2>
-    <form name="kapcsolat" action="./?oldal=urlapelkuld" method="POST">
+    <form name="kapcsolat" action="./?oldal=urlapelkuld" method="POST" onsubmit="return ellenoriz();">
         <div>
             <!--<input type="hidden" name="oldal" value="urlapelkuld">-->
-            <label>Név (minimum 5 karakter): <input type="text" id="nev" name="nev" size="20" maxlength="40" required minlength = "5"></label>
+            <label>Név (minimum 5 karakter): <input type="text" id="nev" name="nev" size="20" maxlength="40"></label>
             <br/>
-            <label>E-mail (kötelező): <input type="text" id="email" name="email" size="30" maxlength="40" required pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"></label>
+            <label>E-mail (kötelező): <input type="text" id="email" name="email" size="30" maxlength="40"></label>
             <br/>
-            <label>Üzenet (kötelező): <textarea id="szoveg" name="szoveg" cols="40" rows="10" required></textarea></label>
+            <label>Üzenet (kötelező): <textarea id="szoveg" name="szoveg" cols="40" rows="10"></textarea></label>
             <br/>
             <input id="kuld" type="submit" value="Küld">
+            <button onclick="ellenoriz();" type="button">Ellenőriz</button>
         </div>
     </form>
+    <script src="js/main.js"></script>
